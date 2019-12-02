b7561477a0407da4618ab72fa9de4de927b89130
Soulliom <gevorkbagratyan@gmail.com>
2019-12-02 14:43:39 -0800

    added files

---
+++ include/InputManager.h
@@ -0,0 +1,9 @@
+#ifndef INPUT_M_H
+#define INPUT_M_H
+
+//Saves SceneState and updates when different scenes are loaded
+class InputManager {
+
+};
+
+#endif 
---
+++ include/SceneManager.h
@@ -0,0 +1,9 @@
+#ifndef SCENE_M_H
+#define SCENE_M_H
+
+//Contains different scenes
+class SceneManager {
+
+};
+
+#endif 
