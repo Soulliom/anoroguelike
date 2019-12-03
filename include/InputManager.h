cf4f249698cd10e49600c300c0aefcec760f7e30
Soulliom <gevorkbagratyan@gmail.com>
2019-12-02 23:52:45 -0800

    added Pause func

--- include/InputManager.h
+++ include/InputManager.h
@@ -1,9 +1,15 @@
 #ifndef INPUT_M_H
 #define INPUT_M_H
 
+#include "DEFINITIONS.h"
+
 //Saves SceneState and updates when different scenes are loaded
 class InputManager {
+public:
+	//Text - What text will be displayed. Key - what key is needed to press, NULL for none
+	void Pause(std::string text, const unsigned int key);
 
+private:
 };
 
 #endif 
