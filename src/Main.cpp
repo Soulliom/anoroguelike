ee101df6d141987bf6e924b03478038fd7fa109a
Soulliom <gevorkbagratyan@gmail.com>
2019-12-02 23:50:45 -0800

    Title Screen

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "../include/SceneManager.h"
+#include "../include/InputManager.h"
+#include "../include/DEFINITIONS.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+	/* Init */
+	SceneManager scene;
+	InputManager input;
+
+	/* Title */
+	scene.Title();
+	input.Pause("Press the [Space] key to Continue.", SPAC); 
+
+	/* Settings */
 }
