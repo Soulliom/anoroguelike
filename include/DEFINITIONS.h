966e5146a75193290eb5bf7af5797b391727872b
Soulliom <gevorkbagratyan@gmail.com>
2019-12-02 23:52:20 -0800

    Compiled all includes and defines into one header

---
+++ include/DEFINITIONS.h
@@ -0,0 +1,17 @@
+#ifndef DEFINITIONS
+#define DEFINITION
+
+//Library Includes
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+
+//Definitions
+#define SPAC 32
+#define WKEY 119 
+#define AKEY 97
+#define SKEY 115
+#define DKEY 100
+
+#endif // !DEFINITIONS
