aedaaa34cee0d99fc85c849d9029095ca4ae90d4
Soulliom <gevorkbagratyan@gmail.com>
2019-12-02 23:52:35 -0800

    Title Scene

--- include/SceneManager.h
+++ include/SceneManager.h
@@ -1,9 +1,19 @@
 #ifndef SCENE_M_H
 #define SCENE_M_H
 
+#include "DEFINITIONS.h"
+
 //Contains different scenes
 class SceneManager {
+public:
+	//Console Init
+	SceneManager();
+	//Title Scene
+	void Title();
 
+	HWND consoleWindow = GetConsoleWindow();
+	HANDLE textConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+private:
 };
 
 #endif 
