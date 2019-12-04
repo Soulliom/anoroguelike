bc991a36567b33e996977a44a7cf45fc5ea2313e
Soulliom <gevorkbagratyan@gmail.com>
2019-12-03 20:28:46 -0800

    cleaned code

--- include/InputManager.h
+++ include/InputManager.h
@@ -6,8 +6,8 @@
 //Saves SceneState and updates when different scenes are loaded
 class InputManager {
 public:
-	//Text - What text will be displayed. Key - what key is needed to press, NULL for none
-	void Pause(std::string text, const unsigned int key);
+	//Text- What text will be displayed. Seconds- length of delay. Key- what key is needed to press to continue, NULL for none.
+	void Pause(std::string text, const unsigned int seconds, const unsigned int key);
 
 private:
 };
